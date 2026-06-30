c80dbe8d648e9e155565e8028a3df0ba21681298
Evaristekanova <dusingizimanaevariste3@gmail.com>
2026-06-30 14:57:10 +0200

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center">
+      <h1 className="text-red-500">Women Concern</h1>
+    </div>
+  );
+}
